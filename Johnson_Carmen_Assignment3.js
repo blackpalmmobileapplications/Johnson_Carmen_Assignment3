d79f107adc3d621a3c3c42db0426576e0e456874
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-17 18:32:20 -0700

    add to js file

--- Johnson_Carmen_Assignment3.js
+++ Johnson_Carmen_Assignment3.js
@@ -1,3 +1,15 @@
 alert("JavaScript works!");
+// Carmen Earl Johnson
+//SDI Project 3
+//05/17/2012
 
-// I have made a change
+
+//Global variables
+var yell = function(message) {console.log(message);
+};
+ 
+var tanks = 4
+var crew = 16  
+var vehi = tanks
+    tanknames = ["Men of War", "Duds", "commander", "master gunner"];
+
