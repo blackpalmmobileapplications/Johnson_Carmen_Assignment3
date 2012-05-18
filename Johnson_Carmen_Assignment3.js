fe9f8f9e5df7c31de6e892fcfd141eb40d71f19d
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-17 18:50:46 -0700

    commit to js

--- Johnson_Carmen_Assignment3.js
+++ Johnson_Carmen_Assignment3.js
@@ -13,3 +13,18 @@ var crew = 16
 var vehi = tanks
     tanknames = ["Men of War", "Duds", "commander", "master gunner"];
 
+//object for top aka First Sargeant
+
+var top = {"rank": "First Sargeant",
+           "age" :  40,
+           "officer": false,
+           "name": "commander",
+            downrange:function (mission) {
+                yell("We are about to show " + tanknames[1] + " how to hit some target!");
+},
+
+          smokeGen: function() 
+               { yell("All companies!! Start your engines!");
+} };
+
+//Procedure
